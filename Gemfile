ed119f1a8dc2797f8c22763303c43e60e9d920eb
baeriswr <roman.baeriswyl@unifr.ch>
2013-12-13 12:05:40 +0100

    subscriptions working: httparty

--- Gemfile
+++ Gemfile
@@ -43,6 +43,8 @@ end
 
 gem 'rails_12factor', group: :production
 
+gem 'httparty'
+
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,116 @@
+require 'httparty'
+
 class SessionsController < ApplicationController
   include SessionsHelper
+  include HTTParty
+  #format :xml
+  #base_uri 'http://diufvm31.unifr.ch:8090/CyberCoachServer/resources/users/'
+  #default_params :authorization => 'xxxxxxx'
+
+
+  def initializeAfterLogin
+    self.class.base_uri "http://diufvm31.unifr.ch:8090/CyberCoachServer/resources/"
+    self.class.basic_auth session[:current_user_link_username], session[:current_user_link_password]
+    self.class.headers({'X-API-VERSION' => '1',
+                        'Accept' => 'application/json'})
+  end
+
+  def create_sport(sport)
+    self.class.headers({ 'X-API-VERSION' => '1',
+                         'Accept' => 'application/json',
+                         'Content-Type' => "application/x-www-form-urlencoded"})
+
+    options = {
+        :body => {"publicvisible" => "2"}}
+
+
+    urlsport = '/users/' +  session[:current_user_link_username] + '/' + sport.name
+
+    self.class.put(urlsport, options)
+  end
+
+  def check_sport
+    ##################################################
+    # check all listed sport_links for a user:       #
+    # put them on cybercoach as a sport subscription #
+    ################################################ #
+    @sports
+    #@sports.each do |s|
+      if (SubscriptionLink.find_by_user_link_id(session[:current_user_link_id]) != nil)    #user has at least one subscription
+        SubscriptionLink.find_all_by_user_link_id(session[:current_user_link_id]).each do |sub|
+          sport=SportLink.find(sub.sport_link_id)
+          create_sport(sport)
+        end
+
+        #@sports.each do |s|
+        #   create_sport(s)
+        #end
+
+      end
+
+
+    #end
+
+
+
+
+
+  end
 
 =begin
-  def new
-    @titre = "Login"
+@exercises.each do |e|
+      newentry = 1
+      if (SportLink.find_by_name(e) != nil) #this sport exists on cybercoach (or at least local)
+        sport =  SportLink.find_by_name(e)
+        if (SubscriptionLink.find_by_sport_link_id(sport.id) != nil) # there is at least one subscription for this sport
+          SubscriptionLink.find_all_by_sport_link_id(sport.id).each do |sub| #look at every subscription that matches the sport
+            if(sub.user_link_id == session[:current_user_link_id])    #check if it is for the current user
+              newentry = 0       # entry already exists, dont create a new one later
+            end
+
+          end
+        end
+        if (newentry == 1)
+          SubscriptionLink.create(:user_link_id => session[:current_user_link_id], :sport_link_id => sport.id)
+        end
+
+      end
+
+
+    end
+
+
+
+
+
+=end
+
+
+=begin
+  class Webservice
+    include HTTParty
+    format :xml
+    base_uri 'mysite.com'
+    default_params :authorization => 'xxxxxxx'
+
+    def self.add_candidate(first_name,last_name,email,gender)
+      post('/test.xml', :body => "")
+    end
   end
 =end
+=begin
+  def self.add_sport(sport,user)
+    put('/test.xml', :body => {
+        :Candidate => {
+            :FirstName => first_name,
+            :LastName  => last_name,
+            :Email     => email,
+            :Gender    => gender,
+        }
+    }
+  end
+=end
+
 
 
   def create
@@ -32,7 +137,10 @@ class SessionsController < ApplicationController
         # no fatsecret profile yet, dont request a session key
       else
         sign_in_fatsecret(params)
+
         getexercises(params)
+        initializeAfterLogin
+        check_sport
       end
 
       #redirect_to user_link
